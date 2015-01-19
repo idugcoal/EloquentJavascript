961b84f4829d7bf32642285dd293c1caa2c312bc
idugcoal <idugcoal@gmail.com>
2015-01-19 15:09:27 -0800

    Eloquent Javascript
    Recursion

--- 03_Recursion.js
+++ 03_Recursion.js
@@ -1,8 +1,13 @@
 function isEven(x) {
-  return x % 2 == 0 ? true : false;
+  if(x < 0)			// if the input is negative, change it to positive
+    x *= -1;
+  if(x == 0)		// if the input is zero, our number is even
+    return true;
+  else if(x == 1)	// if the input is one, our input is odd
+    return false;
+  else				// if the input isn't zero or one, subtract two and try again
+    return isEven(x - 2);
 }
-
-
 console.log(isEven(50));
 // → true
 console.log(isEven(75));
